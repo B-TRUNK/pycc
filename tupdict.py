cd897f67eb9913da2086c77028ef83dd864a3c66
abanoyb_boshra <abanoub.boshra.anis.com>
2025-09-01 17:18:38 +0300

    tuples

---
+++ tupdict.py
@@ -0,0 +1,12 @@
+#Working with Tuples
+
+
+#Tuples values cannot be changed
+#But if the tuple is assigned to new values it will work
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+dimensions = (400, 100)
+print(dimensions[0])
+print(dimensions[1])
